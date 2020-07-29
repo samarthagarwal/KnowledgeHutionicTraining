81ee450932466fcdc9f33aa1fefc7c0bb09c4a3e
Samarth Agarwal <samarthagarwal@live.com>
2020-07-29 16:14:51 +0530

    Added 4 controllers

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,10 @@
 import { Component } from "@angular/core";
+import {
+  LoadingController,
+  ToastController,
+  AlertController,
+  ActionSheetController,
+} from "@ionic/angular";
 
 @Component({
   selector: "app-home",
@@ -6,62 +12,109 @@ import { Component } from "@angular/core";
   styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-  superheroes: any[] = [
-    {
-      name: "Batman",
-      strength: 45,
-      id: 12,
-    },
-    {
-      name: "Superman",
-      strength: 95,
-      id: 13,
-    },
-    {
-      name: "Flash",
-      strength: 55,
-      id: 14,
-    },
-    {
-      name: "Spiderman",
-      strength: 65,
-      id: 15,
-    },
-    {
-      name: "Phantom",
-      strength: 85,
-      id: 16,
-    },
-    {
-      name: "Tarzan",
-      strength: 95,
-      id: 17,
-    },
-  ];
+  constructor(
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController,
+    private alertCtrl: AlertController,
+    private actionsheetCtrl: ActionSheetController
+  ) {}
 
-  constructor() {}
+  async showLoading() {
+    let loading = await this.loadingCtrl.create({
+      spinner: "dots",
+      message: "Loading, please wait",
+    });
 
-  getColor(strength: number) {
-    if (strength < 25) {
-      return "primary";
-    } else if (strength < 50) {
-      return "secondary";
-    } else if (strength < 75) {
-      return "success";
-    } else {
-      return "danger";
-    }
+    loading.present();
+
+    // Emulated 5 seconds task
+    setTimeout(() => {
+      loading.dismiss();
+    }, 5000);
+
+    // any code here will be after
   }
 
-  addSuperhero(terms, id, name, strength) {
-    if (terms == null || terms == false) {
-      return;
-    }
-
-    this.superheroes.push({
-      name: name,
-      id: id,
-      strength: strength,
+  async showToast() {
+    let toast = await this.toastCtrl.create({
+      header: "New Message",
+      message: "You have a new message!",
+      duration: 3000,
+      position: "bottom",
+      buttons: [
+        {
+          text: "OK",
+          handler: () => {
+            console.log("You pressed okay!");
+          },
+        },
+      ],
     });
+
+    toast.present();
+  }
+
+  async showAlert() {
+    let alert = await this.alertCtrl.create({
+      header: "WiFi Password",
+      subHeader: "Please enter the new password",
+      message: "Your password seems to have been changed recently.",
+
+      inputs: [{
+        name: "password",
+        type: "password",
+        placeholder: "12345678"
+      }],
+
+      buttons: [
+        {
+          text: "Submit",
+          handler: (data) => {
+            console.log("You pressed submit");
+            
+            // prevent alert from dismissing
+            if(data.password == null || data.password != "12345678") {
+              return false;
+            }
+
+            
+          },
+        },
+        {
+          text: "Cancel",
+          handler: () => {
+            console.log("You pressed cancel");
+          },
+        },
+      ],
+    });
+
+    alert.present();
+  }
+
+  async showActionsheet() {
+    let actionsheet = await this.actionsheetCtrl.create({
+      header: "Select an option",
+      subHeader: "Choose a country",
+      buttons: [{
+        text: "UK",
+        handler: () => {
+
+        }
+      }, {
+        text: "USA",
+        role: "selected",
+        handler: () => {
+          
+        }
+      }, {
+        text: "India",
+        handler: () => {
+          
+        }
+      }]
+    });
+
+    actionsheet.present();
   }
 }
